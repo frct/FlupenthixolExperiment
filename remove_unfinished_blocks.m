12df3e22e189676799b762f498dd0fc86a00528c
frct <frct@users.noreply.github.com>
2019-11-26 12:08:52 +0000

    Delete remove_unfinished_blocks.m

--- remove_unfinished_blocks.m
+++
@@ -1,7 +0,0 @@
-% I want a function that goes through S1-12 and S13-24 and eliminates
-% blocks with shortened lengths
-
-function A=remove_unfinished_blocks(A)
-
-lines=lines_to_be_deleted(A);
-A(lines,:)=[];
